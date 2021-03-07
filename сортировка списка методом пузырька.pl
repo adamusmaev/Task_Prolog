602629d71435ea75b9fcd693da52f5538d632a1d
adamusmaev <adam.usmaev2000@mail.ru>
2021-03-07 13:21:43 +0300

    Prolog

--- сортировка списка методом пузырька.pl
+++ сортировка списка методом пузырька.pl
@@ -1,30 +1,18 @@
+mySort(List, List):-
+  change(List, List1),
+  List = List1, !.
 
+mySort(List, SortedList):-
+  change(List, SortedPart),
+  mySort(SortedPart, SortedList).
 
+change([], []):-!.
 
+change([Head], [Head]):-!.
 
+change([First, Second|Tail], [Second|ListWithMaxEnd]):-
+    First > Second, !,
+    change([First|Tail], ListWithMaxEnd).
 
-
-
-/*mySort1(List1, List) :- mySort1(List1, [], List).
-mySort1([X|List1], List2, List) :- mySort2(X, List1, List2).
-mySort2(X, [Y|List1], List2) :- (X > Y, 
-                            mySort2(X, List1, [Y|List2]));
-                            mySort2(X, List1, List2).
-
-mySort2(X, [], []) :- !.                            
-/*mySort2(X, [Y|List1], List2) :- mySort2(X, List1, List2).                        
-
-
-compar(X, [Y|Tail], List2) :-  change(List2, [X|Tail]).
-change(List, List) :- !.
-*/
-
-
-
-/*
-
-compar(X, [Y|Tail], List2) :- X>Y, 
-                            change(List2, [X|Tail]).
-change(List, List) :- !.
-
-*/
+change([First, Second|Tail], [First|ListWithMaxEnd]):-
+  change([Second|Tail], ListWithMaxEnd).
