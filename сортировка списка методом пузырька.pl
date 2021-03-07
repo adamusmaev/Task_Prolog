98e9f495fac851a7c901a1d351d9f6a9f3a3c108
adamusmaev <adam.usmaev2000@mail.ru>
2021-03-07 13:21:01 +0300

    Prolog

--- сортировка списка методом пузырька.pl
+++ сортировка списка методом пузырька.pl
@@ -1,3 +1,30 @@
-mySort([X|Tail]) :- compar(X, Tail), mychange(X, Tail).
-compar(X, [Y|Tail]) :- X > Y.
-mychange(X, [Y|Tail]) :- mySort(Y, )
+
+
+
+
+
+
+
+/*mySort1(List1, List) :- mySort1(List1, [], List).
+mySort1([X|List1], List2, List) :- mySort2(X, List1, List2).
+mySort2(X, [Y|List1], List2) :- (X > Y, 
+                            mySort2(X, List1, [Y|List2]));
+                            mySort2(X, List1, List2).
+
+mySort2(X, [], []) :- !.                            
+/*mySort2(X, [Y|List1], List2) :- mySort2(X, List1, List2).                        
+
+
+compar(X, [Y|Tail], List2) :-  change(List2, [X|Tail]).
+change(List, List) :- !.
+*/
+
+
+
+/*
+
+compar(X, [Y|Tail], List2) :- X>Y, 
+                            change(List2, [X|Tail]).
+change(List, List) :- !.
+
+*/
